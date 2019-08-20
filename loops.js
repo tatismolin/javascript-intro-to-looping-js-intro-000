2e0e2d152a466713017798dc30d9ed5c7de16d6f
immutable-illustrator-3998 <tatismolin@gmail.com>
2019-08-20 23:57:54 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 function forLoop(array){
-  for(i = 0; i < array.length; i++)
-  if(i === 0){
+  for(i = 0; i < forLoop.length; i++)
+  if(i === 1){
     return "I am 1 strange loop."''
   } else {
     return`I am ${i} strange loops.`;

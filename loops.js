1f45b79d95c1d9571c22e971e425c7bf25c74958
immutable-illustrator-3998 <tatismolin@gmail.com>
2019-08-21 00:03:37 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,8 +1,8 @@
 function forLoop(array){
   for(i = 0; i < 25; i++)
   if(i === 1){
-    forLoop.push(i + "I am 1 strange loop.");
+    return forLoop.push(i + "I am 1 strange loop.");
   } else {
-    forLoop.push(`I am ${i} strange loops.`);
+    return forLoop.push(`I am ${i} strange loops.`);
   }
 }
